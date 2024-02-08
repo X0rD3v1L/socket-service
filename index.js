8bfd6b1dc78667853b51ece8e0cf9ad3b30c01b6
Benarjee Sambangi <46685302+X0rD3v1L@users.noreply.github.com>
2024-02-08 22:37:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,11 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http, {cors: {origin: '*',methods: ["GET", "POST"]}});
 const port = process.env.PORT || 3000;
 
-let arr=[]
 
 io.on("connection", (socket) => {
   
   socket.on("join_room", (data) => {
     socket.join(data);
-    if(data.room != null){
-	arr.push(data.room)
-	if(arr.length == 2){
-		socket.emit("both_players_joined",2);
-	}
-    }
   });
 
   socket.on("deployed_contract_address", (data) => {
