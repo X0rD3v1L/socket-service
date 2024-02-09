ef9fc646d9c5453b9d4dde2d78a0d31b1c19224a
Benarjee Sambangi <46685302+X0rD3v1L@users.noreply.github.com>
2024-02-09 12:18:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http, {cors: {origin: '*', methods: ["GET", "POST"]}});
+const io = require('socket.io')(http, {cors: {origin: '*',methods: ["GET", "POST"]}});
 const port = process.env.PORT || 3000;
 
+let totalConnections = 0;
 io.on("connection", (socket) => {
-  let roomConnections = {};
-
+  
   socket.on("join_room", (data) => {
-    socket.join(data.room);
-    if (!roomConnections[data.room]) {
-      roomConnections[data.room] = 0;
-    }
-    roomConnections[data.room]++;
-
-    if (roomConnections[data.room] === 2) {
-      io.to(data.room).emit("both_players_joined", data);
+    socket.join(data);
+    totalConnections += 1;
+    if(totalConnections == 2){
+	socket.to(data.room).emit("both_players_joined", data);
     }
   });
 
@@ -26,18 +22,11 @@ io.on("connection", (socket) => {
     socket.to(data.room).emit("received_opponent_move", data);
   });
   
-  socket.on("initiator_move", (data) => {
-    socket.to(data.room).emit("received_initiator_move", data);
+  socket.on("initator_move", (data) => {
+    socket.to(data.room).emit("received_initator_move", data);
   });
 
-  socket.on("disconnect", () => {
-    // Decrease the connection count when a user disconnects
-    Object.keys(roomConnections).forEach(room => {
-      if (roomConnections[room] > 0) {
-        roomConnections[room]--;
-      }
-    });
-  });
+
 });
 
 http.listen(port, () => {
